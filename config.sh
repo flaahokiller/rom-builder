f75d10180c6e57964383a951af43747c3fa4eae1
Flashokiller <75789819+flaahokiller@users.noreply.github.com>
2021-04-04 14:18:54 +0530

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-GITHUB_USER=Akianonymus
-GITHUB_EMAIL=anonymus.aki@gmail.com
+GITHUB_USER=flaahokiller
+GITHUB_EMAIL=flashokiller@gmail.com
 
 KBUILD_BUILD_USER="Aki"
 KBUILD_BUILD_HOST="A_DEAD_PLANET"
 
-export oem=motorola
-export device=harpia
+export oem=xiaomi
+export device=ysl
 
-ROM="PixelExperience"
-manifest_url=""
+ROM="Palladium"
+manifest_url="https://github.com/Palladium-OS/platform_manifest.git"
 export rom_vendor_name="palladium" # This represent the nams used by different rom vendors, Ex - aosp_harpia-userdebug, aosp is vendor name.
 branch="11"
 
